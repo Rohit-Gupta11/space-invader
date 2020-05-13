96fbc5e1fcc4216101bd7e599a6412935f80b65d
Rohit Gupta <grohit7711@gmail.com>
2020-05-13 20:18:25 +0530

    add player

--- main.py
+++ main.py
@@ -12,8 +12,21 @@ pygame.display.set_icon(icon)
 #------------------------Global variables-----------------------------------------------------------
 running = True
 
+#------------------------Player---------------------------------------------------------------------
+player_img = pygame.image.load("images/player.png")
+px = 340
+py = 480
+def player(x,y):
+    screen.blit(player_img,(x,p))
+
 #------------------------Game loop------------------------------------------------------------------
 while running:
+
+    screen.fill((0,114,114))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player(px,py)
+    pygame.display.update()
